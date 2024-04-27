2344fe7c37f80edd3a8543d3debd9e33bf450060
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2024-04-27 17:27:50 +0530

    chore: :construction: Added envs

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    //   DATABASE_URL: z.string().url(),
+    DATABASE_URL: z.string().url(),
     CLERK_SECRET_KEY: z.string(),
   },
   client: {
@@ -13,8 +13,8 @@ export const env = createEnv({
   },
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-    //   DATABASE_URL: process.env.DATABASE_URL,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
